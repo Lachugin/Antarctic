3f93d2f3941fe6f1b63feb3633c7f2adccc67cd9
Vladimir Lachugin <Vladimirla4ygin@yandex.ru>
2022-09-07 06:52:28 +0300

    устранения багов 1

--- source/js/modules/modals/menu.js
+++ source/js/modules/modals/menu.js
@@ -5,6 +5,7 @@ export const getMenu = () => {
   const headerBtn = document.querySelector('[data-btn]');
   const headerBody = document.querySelector('[data-body]');
   const headerItems = document.querySelectorAll('[data-item]');
+  const headerOverplay = document.querySelector('[data-overplay]');
 
   headerBtn.classList.remove('header__btn--active');
   headerNav.classList.remove('nav--is-open-no-js');
@@ -20,6 +21,7 @@ export const getMenu = () => {
     headerLogo.classList.toggle('logo__active');
     headerList.classList.toggle('nav__list--active');
     headerBody.classList.toggle('body-fixed');
+    headerOverplay.classList.toggle('header__overplay');
   });
 
   headerItems.forEach((el) => {
@@ -30,6 +32,17 @@ export const getMenu = () => {
       headerLogo.classList.remove('logo__active');
       headerList.classList.remove('nav__list--active');
       headerBody.classList.remove('body-fixed');
+      headerOverplay.classList.toggle('header__overplay');
     });
+  });
+
+  headerOverplay.addEventListener('click', function (e) {
+    e.preventDefault();
+    headerBtn.classList.toggle('header__btn--active');
+    headerNav.classList.toggle('nav__active');
+    headerLogo.classList.toggle('logo__active');
+    headerList.classList.toggle('nav__list--active');
+    headerBody.classList.toggle('body-fixed');
+    headerOverplay.classList.toggle('header__overplay');
   });
 };

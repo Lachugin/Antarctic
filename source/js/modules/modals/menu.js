833894521396a3b9582c59876a4d677e16d01794
Vladimir Lachugin <Vladimirla4ygin@yandex.ru>
2022-09-07 20:21:37 +0300

    устранения багов 1

--- source/js/modules/modals/menu.js
+++ source/js/modules/modals/menu.js
@@ -4,7 +4,6 @@ export const getMenu = () => {
   const headerList = document.querySelector('[data-nav-list]');
   const headerBtn = document.querySelector('[data-btn]');
   const headerBody = document.querySelector('[data-body]');
-  const headerItems = document.querySelectorAll('[data-item]');
   const headerOverplay = document.querySelector('[data-overplay]');
 
   headerBtn.classList.remove('header__btn--active');
@@ -12,7 +11,6 @@ export const getMenu = () => {
   headerNav.classList.remove('nav__active');
   headerLogo.classList.remove('logo__active');
   headerList.classList.remove('nav__list--active');
-  headerBody.classList.remove('body-fixed');
 
   headerBtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -24,20 +22,8 @@ export const getMenu = () => {
     headerOverplay.classList.toggle('header__overplay');
   });
 
-  headerItems.forEach((el) => {
-    el.addEventListener('click', () => {
-      headerBtn.classList.remove('header__btn--active');
-      headerNav.classList.remove('nav--is-open-no-js');
-      headerNav.classList.remove('nav__active');
-      headerLogo.classList.remove('logo__active');
-      headerList.classList.remove('nav__list--active');
-      headerBody.classList.remove('body-fixed');
-      headerOverplay.classList.toggle('header__overplay');
-    });
-  });
-
-  headerOverplay.addEventListener('click', function (e) {
-    e.preventDefault();
+  headerOverplay.addEventListener('click', function (b) {
+    b.preventDefault();
     headerBtn.classList.toggle('header__btn--active');
     headerNav.classList.toggle('nav__active');
     headerLogo.classList.toggle('logo__active');
--- source/js/modules/modals/scroll.js
+++ source/js/modules/modals/scroll.js
@@ -1,8 +1,21 @@
+const headerNav = document.querySelector('[data-nav]');
+const headerLogo = document.querySelector('[data-logo]');
+const headerList = document.querySelector('[data-nav-list]');
+const headerBtn = document.querySelector('[data-btn]');
+const headerBody = document.querySelector('[data-body]');
+const headerOverplay = document.querySelector('[data-overplay]');
+
 export const scroll = () => {
   const smoothLinks = document.querySelectorAll('.nav__item a[href^="#"]');
   for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
       e.preventDefault();
+      headerBtn.classList.remove('header__btn--active');
+      headerNav.classList.remove('nav__active');
+      headerLogo.classList.remove('logo__active');
+      headerList.classList.remove('nav__list--active');
+      headerBody.classList.remove('body-fixed');
+      headerOverplay.classList.remove('header__overplay');
       const id = smoothLink.getAttribute('href');
       document.querySelector(id).scrollIntoView({
         behavior: 'smooth',
